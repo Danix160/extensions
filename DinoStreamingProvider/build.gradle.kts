d84969d8b43a98ada673c6c27512eb8e5cfc2323
Danix160 <tiziotipo18@gmail.com>
2025-07-26 14:28:46 +0200

    Update build.gradle.kts

--- DinoStreamingProvider/build.gradle.kts
+++ DinoStreamingProvider/build.gradle.kts
@@ -1,12 +1,11 @@
-
 group = "com.dinostreaming"
 version = "1.0"
 
-cloudstream("DinoStreaming") {
-    language = "it"
-    description = "Movies and Shows from DinoStreaming"
-    authors = listOf("doGior")
-    tvTypes = listOf("Movie", "TvSeries")
-    iconUrl = "https://dinostreaming.it/favicon.ico"
+cloudstream {
+    language.set("it")
+    description.set("Movies and Shows from DinoStreaming")
+    authors.set(listOf("doGior"))
+    tvTypes.set(listOf("Movie", "TvSeries"))
+    iconUrl.set("https://dinostreaming.it/favicon.ico")
 }
 
